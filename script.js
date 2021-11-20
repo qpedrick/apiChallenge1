e6b645e821f5b0761c0aa9c2899eb7240f699bd9
Quinton Pedrick <77907943+qpedrick@users.noreply.github.com>
2021-11-20 16:41:38 -0600

    reworked project

--- script.js
+++ script.js
@@ -1,59 +1,6 @@
 const baseURL = "https://api.covid19api.com/"
 
-/* Sample Request
-
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-};
-
-fetch(`${baseURL}countries`, requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .then(error => console.log('error', error));
-
-*/
-
-//Drop Down Selector for Country
-
-        /* VanillaJS
-
-        let dropdown = document.getElementById('locality-dropdown');
-        dropdown.length = 0;
-
-        let defaultOption = document.createElement('option');
-        defaultOption.text = 'Choose Country';
-
-        dropdown.add(defaultOption);
-        dropdown.selectedIndex = 0;
-
-        const dropURL = 'https://api.covid19api.com/countries';
-
-        const request = new XMLHttpRequest();
-        request.open('GET', dropURL, true);
-
-        request.onload = function() {
-            if(request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                let option;
-                for (let i = 0; i < data.length; i ++) {
-                    option = document.createElement('option');
-                    option.text = data[i].name;
-                    option.value = data[i].abbreviation;
-                    dropdown.add(option);
-                }
-            } else {}
-        }
-
-        request.onerror = function() {
-            console.error('An error occured fetching the JSON from the ' + dropURL);
-        };
-
-        request.send()
-
-        */
-
-/* Fetch */
+/* Fetch for drop down */
 
 let dropdown = document.getElementById('locality-dropdown');
 dropdown.length = 0;
@@ -84,17 +31,41 @@ fetch(dropURL)
                     option.text = data[i].Country;
                     option.value = data[i].Slug;
                     dropdown.add(option);
+                    let country = option.innerHTML
+                    //console.log(country);
                 }
             });
         }
     )
     .catch(function(err) {
         console.error('Fetch error -', err);
-    })
+    });
 
-fetch(baseURL)
-    .then(
-        response => {
-            
-        }
-    )
+const searchForm = document.querySelector('form')
+const submitBtn = document.querySelector('.submit')
+searchForm.addEventListener('submit',fetchResults);
+
+function fetchResults(e) {
+    e.preventDefault()
+
+url = `${baseURL}summary`
+fetch(url)
+    .then(function(result) {
+    console.log(result)
+    return result.json()
+    })
+    .then (function(json) {
+        console.log(json);
+        displayResults(json);
+    })
+}
+
+function displayResults(json) {
+    let results
+    for (let i = 0; i < data.length; i ++) {
+        results = document.createElement('results');
+        results.text = data[i].Country;
+        results.value = data[i].TotalDeaths;
+        console.log(results.value);
+    }
+    }
